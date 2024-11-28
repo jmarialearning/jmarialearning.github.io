5556500ec50b33b51e8d941750ddcf669a3810f5
Jmarialearning <46451400+jmarialearning@users.noreply.github.com>
2024-11-28 20:33:42 +0100

    Change test to power

---
+++ repo/src/power.js
@@ -0,0 +1,5 @@
+// Esto es para probar JavaScript y hacer tests. Proggrama que eleva según lo que el usuario dice.
+var x = prompt("Base");
+var y = prompt("Exponente");
+var z = Math.pow(x,y);
+console.log(z)
--- repo/src/test.js
+++
@@ -1,4 +0,0 @@
-// Esto es para probar JavaScript y hacer tests.
-var x = 2;
-var y = 3;
-var z = Math.Pow(x,y);
