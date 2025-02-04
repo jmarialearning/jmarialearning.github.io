42f5d27bff4f0e2476cf328eac0f6d901e41dc9c
Jmarialearning <46451400+jmarialearning@users.noreply.github.com>
2025-02-04 17:15:47 +0100

    Minor fix on comments

--- repo/src/power.js
+++ repo/src/power.js
@@ -1,4 +1,4 @@
-// Esto es para probar JavaScript y hacer tests. Proggrama que eleva según lo que el usuario dice.
+// Esto es para probar JavaScript y hacer tests. Programa que eleva según lo que el usuario dice.
 var x = prompt("Base");
 var y = prompt("Exponente");
 var z = Math.pow(x,y);
