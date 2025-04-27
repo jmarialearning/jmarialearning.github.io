473594f9cfb401f84af7f77f04e68f4a689eb27e
Jmarialearning <46451400+jmarialearning@users.noreply.github.com>
2025-04-27 17:56:31 +0200

    Continue act1 at cnumerico

--- repo/acts/unir/fis/cnumerico/act1/main.cpp
+++ repo/acts/unir/fis/cnumerico/act1/main.cpp
@@ -1,7 +1,18 @@
-// Cálculo numérico. Crear dos funciones llamadas _muller_ y _ridders_ para trabajar con estos m�todos.
+// Cálculo numérico. Crear dos funciones llamadas _muller_ y _ridders_ para trabajar con estos metodos.
 #include <iostream>
 #include "muller.h"
 #include "ridders.h"
+#include <conio.h>
+using namespace std;
 int main(){
-    std::cout << "Hola";
+    cout << "Hola\n\n";
+    double a,b;
+    int its;
+    printf("Extremo inferior: ");
+    scanf("%f", &a);
+    printf("\nExtremo superior: ");
+    scanf("%f", &b);
+    printf("\nIteraciones del método de Muller: ");
+    scanf("%d", &its);
+    muller(a,b,its);
 }
--- repo/acts/unir/fis/cnumerico/act1/muller.h
+++ repo/acts/unir/fis/cnumerico/act1/muller.h
@@ -4,7 +4,7 @@ using namespace std;
 /*
 Muller. Argumentos:
     a y b son los extremos del intervalo.
-    f es una función con tres argumentos:
+    f es una funciÃ³n con tres argumentos:
         punto a evaluar
         referencia al valor en el punto
         referencia a la primera derivada en el punto
@@ -12,16 +12,53 @@ Muller. Argumentos:
     its son las iteraciones
     error implica el error del proceso
 */
-void muller(double &a, double &b, void *f(double, double &, double &), double &c, int &its, int &error){
-    //Declaramos la precisión que queremos.
-    const double precision = 1.0e-14;
-    const double maxits = 200; // Número máximo de iteraciones que haremos.
+double f(double x){
+        return 1*pow(x,3) + 2;
+    }
+int signo(double num){
+    if (num > 0){
+        return 1;
+    }
+    else{
+        if (num < 0){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
+void muller(double a, double b, int its){
     // Si a es mayor a b, los cambiamos para que a sea el extremo con menor x.
     if (a > b){
         swap(a,b);
     }
+    for(int i = 0;i <= its;i++){
     // Hemos de conseguir un punto medio entre a y b.
     double c = (a+b)/2;
-    // Con a, b y c podemos sacar una parábola que pase por esos 3 puntos.
+    // Con a, b y c podemos sacar una parÃ¡bola que pase por esos 3 puntos.
     // y = px^2+qx+r debe pasar por a, b y c.
+    double gamma = (a-c)/(b-a);
+    double p = (gamma*f(b)-(1+gamma)*f(a)+f(c))/(gamma*pow((b-a),2)*(1+gamma));
+    double r = f(a);
+    double q = (f(b)-f(a)-p*pow(b-a,2))/(b-a);
+
+    double solucion = a - (2*r)/(q+signo(q)*sqrt(pow(q,2)-4*p*r));
+    // Ya tenemos la soluciÃ³n. Veamos como se distribuye.
+    if (solucion > a){
+        c = b;
+        b = solucion;
+    }
+    else{
+        if (solucion < a){
+            // Por seguridad.
+            double c2 = c;
+            double a2 = a;
+            b = solucion;
+            c2 = a;
+            a2 = c;
+        }
+    }
+    printf("%f, %f y %f",f(a),f(b),f(c));
+    }
 }
