ada24f0cb1854d365451d959e3310ce10fab6850
Jmarialearning <46451400+jmarialearning@users.noreply.github.com>
2025-05-01 20:20:58 +0200

    Continue working on ridders

--- repo/acts/unir/fis/cnumerico/act1/nuevo/ridders.cpp
+++ repo/acts/unir/fis/cnumerico/act1/nuevo/ridders.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-float funcion(float x){
+float f(float x){
     return 2*x;
 }
 int main(){
     int iteraciones;
-    
+    float extinf, extsup;
+    printf("Extremo inferior: ");
+    scanf("%f", &extinf);
+    printf("Extremo superior: ");
+    scanf("%f", &extsup);
+    float pmedio = (extinf + extsup) / 2;
+    printf("\nExtremo inferior: %f", extinf);
+    printf("\nExtremo superior: %f", extsup);
+    printf("\nPunto medio: %f", pmedio);
+    printf("\nValores de la función en esos puntos: %f, %f y %f", f(extinf), f(extsup), f(pmedio));
+    if(f(extinf)*f(extsup) > 0){
+        printf("\nModifica los  extremos para que haya un cambio de signo entre ellos.\n");
+        return 1;
+    }
+    for(iteraciones = 1; iteraciones <= 25; iteraciones++){
+        
+    }
 }
