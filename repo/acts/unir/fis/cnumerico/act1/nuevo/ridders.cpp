a9276f592bd793438d4dbb44b5ca1316d3d65bd8
Jmarialearning <46451400+jmarialearning@users.noreply.github.com>
2025-05-02 10:09:50 +0200

    Finish ridders, add pdf with instructions and make report

--- repo/acts/unir/fis/cnumerico/act1/nuevo/ridders.cpp
+++ repo/acts/unir/fis/cnumerico/act1/nuevo/ridders.cpp
@@ -11,7 +11,7 @@ int signo(float num){
     return 0;
 }
 float f(float x){
-    return 2*x;
+    return pow(x,2) - 9;
 }
 int main(){
     int iteraciones;
@@ -26,10 +26,35 @@ int main(){
     printf("\nPunto medio: %f", pmedio);
     printf("\nValores de la función en esos puntos: %f, %f y %f", f(extinf), f(extsup), f(pmedio));
     if(f(extinf)*f(extsup) > 0){
-        printf("\nModifica los  extremos para que haya un cambio de signo entre ellos.\n");
+        printf("\nModifica los  extremos para que haya un cambio de signo entre ellos. Es posiible que el intervalo sea demasiado grande.\n");
         return 1;
     }
     for(iteraciones = 1; iteraciones <= 25; iteraciones++){
-        float cuartoPunto = pmedio + (pmedio - extinf) * (signo(f(extinf) - f(extsup)) * f(pmedio));
+        float cuartoPunto = pmedio + (pmedio - extinf) * (signo(f(extinf) - f(extsup)) * f(pmedio)) / sqrt(pow(f(pmedio),2) - f(extinf) * f(extsup));
+        if (f(cuartoPunto) * f(pmedio) < 0){
+            extinf = pmedio;
+            extsup = cuartoPunto;
+            if(extinf > extsup){
+                swap(extinf,extsup);
+            }
+        }
+        else{
+            if (f(extsup) * f(cuartoPunto) < 0){
+                extinf = extsup;
+                extsup = cuartoPunto;
+                if(extinf > extsup){
+                    swap(extinf,extsup);
+                }
+            }
+            else{
+                if (f(extinf) * f(cuartoPunto) < 0){
+                    extsup = cuartoPunto;
+                    if(extinf > extsup){
+                        swap(extinf,extsup);
+                    }
+                }
+            }
+        }
+        printf("\n%f", cuartoPunto);
     }
-}
+}   
