225e4ddc45e5fd378cfa776bbf21416f6dc84a45
Jmarialearning <46451400+jmarialearning@users.noreply.github.com>
2025-05-11 11:50:56 +0200

    Begin working on act2 at cnumerico

---
+++ repo/acts/unir/fis/cnumerico/act2/funcion.hpp
@@ -0,0 +1,10 @@
+#ifndef FUNCION_HPP
+#define FUNCION_HPP
+#include <cmath>
+double f(double x){
+    return 2 * x;
+}
+double df(double x){
+    return 2;
+}
+#endif /*FUNCION_HPP*/
---
+++ repo/acts/unir/fis/cnumerico/act2/newtonsys.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cmath>
+#include "funcion.hpp"
+void newtonND(double extinf, double extsup, double aproxInicial, int unsigned iteraciones){
+    // Código aquí.
+}
