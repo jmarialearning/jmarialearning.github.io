3b9034fef176c4f53f320cf9d0810b74054656bc
Jmarialearning <46451400+jmarialearning@users.noreply.github.com>
2025-05-17 19:51:24 +0200

    Add bookmarks and continue working on multinewton. There's hope.

--- repo/acts/unir/fis/cnumerico/act2/multinewton.hpp
+++ repo/acts/unir/fis/cnumerico/act2/multinewton.hpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <cmath>
+#include <vector> // Para clase std::vector
 #include "funcion.hpp"
 using namespace std;
-int newtonND(double extinf[], double extsup[], double aproxInicial[], int unsigned iteraciones, double eps){
+int newtonND(vector <double> extinf, vector <double> extsup, vector <double> aproxInicial, int unsigned iteraciones, double eps){
     const double epsilon = eps;
     // Cambiamos los extremos si procede.
     /*if (extsup < extinf){
@@ -13,6 +14,16 @@ int newtonND(double extinf[], double extsup[], double aproxInicial[], int unsign
         printf("Los extremos no permiten concluir la existencia de un cambio de signo. Escoge otros.\n");
         return 1;
     }*/
-   
-
+    if (extinf[0] > extsup[0]){
+        swap(extinf, extsup);
+    }
+    // Calcular matriz jacobiana.
+    double x = aproxInicial[0];
+    double y = aproxInicial[1];
+    vector <double> funcion = {f1(x,y),f2(x,y)};
+    double jacobiana[2][2] = {2 * x + pow(y,3), 3 * pow(y,2) * x,6 * x * y, 3 * pow(x,2) - 3 * pow(y,2)};
+    double jacobianaInvertida[2][2] = {0,0,0,0};
+    vector <double> aproxSucesiva = aproxInicial - jacobianaInvertida /* * */ funcion;
+    aproxInicial = aproxSucesiva;
+    return 0;
 }
