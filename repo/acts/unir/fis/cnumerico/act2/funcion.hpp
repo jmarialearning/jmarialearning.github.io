0061fab41fbeb6ef70770c6284ff921a97b8c92a
Jmarialearning <46451400+jmarialearning@users.noreply.github.com>
2025-05-17 10:17:05 +0200

    There's no a single component working. Nevertheless, there's hope.

--- repo/acts/unir/fis/cnumerico/act2/funcion.hpp
+++ repo/acts/unir/fis/cnumerico/act2/funcion.hpp
@@ -1,8 +1,11 @@
 #ifndef FUNCION_HPP
 #define FUNCION_HPP
 #include <cmath>
-double f(double x){
-    return 2 * x;
+double f1(double x, double y){
+    return pow(x,2) + x * pow(y,3) - 9;
+}
+double f2(double x, double y){
+    return 3 *  pow(x,2) * y - pow(y,3) - 4;
 }
 double df(double x){
     return 2;
--- repo/acts/unir/fis/cnumerico/act2/multinewton.hpp
+++ repo/acts/unir/fis/cnumerico/act2/multinewton.hpp
@@ -2,17 +2,17 @@
 #include <cmath>
 #include "funcion.hpp"
 using namespace std;
-int newtonND(double extinf, double extsup, double aproxInicial, int unsigned iteraciones){
-    const double epsilon = 1.0e-14;
+int newtonND(double extinf[], double extsup[], double aproxInicial[], int unsigned iteraciones, double eps){
+    const double epsilon = eps;
     // Cambiamos los extremos si procede.
-    if (extsup < extinf){
+    /*if (extsup < extinf){
         swap(extinf,extsup);
-    }
+    }*/
     // Comprobamos los cambios de signo de la función.
-    if (f(extinf) * f(extsup) > 0){
+    /*if (f(extinf) * f(extsup) > 0){
         printf("Los extremos no permiten concluir la existencia de un cambio de signo. Escoge otros.\n");
         return 1;
-    }
-
+    }*/
+   
 
 }
