09f366ea7b2e6eec7488662c528c09d04c11754d
jmarialearning <46451400+jmarialearning@users.noreply.github.com>
2025-05-31 14:56:25 +0200

    Add act3 and begin working on it

---
+++ repo/acts/unir/fis/cnumerico/act3/code/predominanteDiagonal.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+using Matrix = vector<vector<double>>;
+bool predominanteDiagonal(const Matrix &matriz){
+    int n = matriz.size();
+    // Comprobamos si es una matriz cuadrada.
+    for (const auto &fila:matriz){
+        if (fila.size() != n){
+            return false;
+        }
+    }
+    double sumaDiagonal = 0;
+    double sumaNoDiagonal = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j<n; j++){
+            if (i = j){
+                sumaDiagonal +=abs(matriz[i][j]);
+            }
+            else{
+                sumaNoDiagonal += abs(matriz[i][j]);
+            }
+        }
+    }
+    // Definir treshold?
+    return sumaNoDiagonal < sumaDiagonal; //por treshold?
+}
